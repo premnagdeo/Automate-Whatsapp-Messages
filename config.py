b7bf5366cdaee707d10d4b8a64351680fc13f2f1
Prem Nagdeo <premnagdeo@gmail.com>
2020-10-13 20:35:58 +0530

    Sample Config File

---
+++ config.py
@@ -0,0 +1,5 @@
+# Add your Twilio account details
+TWILIO_ACCOUNT_SID = 'your_account_ssid'
+TWILIO_AUTH_TOKEN = 'your_auth_token'
+TWILIO_NUMBER = 'whatsapp:<E.164 formatted phone number>'
+DESTINATION_NUMBER = 'whatsapp:<E.164 formatted phone number>'
