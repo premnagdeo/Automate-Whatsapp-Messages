9b3915c2847b7d0dba8b448012dedac3a48deb4c
Prem Nagdeo <premnagdeo@gmail.com>
2020-10-19 17:53:13 +0530

    Updated schedule

--- local.py
+++ local.py
@@ -36,7 +36,8 @@ def send_message():
 
 # Schedule to send message at noon everyday
 # Docs for schedule: https://schedule.readthedocs.io/
-schedule.every().day.at("12:00").do(send_message)
+schedule.every().hour.do(send_message)
+# schedule.every().day.at("12:00").do(send_message)
 
 while True:
     schedule.run_pending()
